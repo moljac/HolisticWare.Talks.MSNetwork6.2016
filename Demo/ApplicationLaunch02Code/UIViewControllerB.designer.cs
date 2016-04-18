b3bb3a6c71c1202da1b929bb92260d421156a7a1
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-18 07:49:22 +0200

    modified sample

---
+++ Demo/ApplicationLaunch02Code/UIViewControllerB.designer.cs
@@ -0,0 +1,22 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ApplicationLaunch02Code
+{
+	[Register("UIViewControllerB")]
+	partial class UIViewControllerB
+	{
+		void ReleaseDesignerOutlets()
+		{
+		}
+	}
+}
+
